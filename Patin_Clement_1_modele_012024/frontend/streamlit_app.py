998292a83ad47e70c1a3edd06308c53963dfaad3
ClementPatin <clement.patin50@gmail.com>
2024-03-19 13:18:57 +0100

    correction : display error UI

--- Patin_Clement_1_modele_012024/frontend/streamlit_app.py
+++ Patin_Clement_1_modele_012024/frontend/streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import requests
 
-st.title("Air Paradis - twitter sentiment analysis_test CI/CD")
+st.title("Air Paradis - twitter sentiment analysis")
 
 col1, col2, col3 = st. columns(3, gap="large")
-baseInputs = ["I hate my job", "I love donuts", "my shirt is yellow"]
+baseInputs = ["I hate my job", "I love donuts", ""]
 
 for i,(col,baseInput) in enumerate(zip([col1, col2, col3], baseInputs)) :
     with col :
@@ -15,25 +15,27 @@ for i,(col,baseInput) in enumerate(zip([col1, col2, col3], baseInputs)) :
         st.write("")
 
 
-        API_URL = "https://testapip7.azurewebsites.net"
+        API_URL = "http://backend:8000"
 
         response = requests.post(API_URL+"/predict", json={"text":tweet}).json()
         # response = requests.post(API_URL+"/predict?text="+tweet)
 
-        st.write("Score :")
-        st.progress(float(response["score"]), text = response["score"])
-        st.write("")
-        st.write("")
+        if "score" not in response.keys() :
+            st.write(":exclamation: Error :exclamation: :")
+            st.write("Detail : "+response["detail"])
 
-        st.write("Sentiment :")
-        if response["sentiment"] == "positive" :
-            st.header(":smile:")
         else :
-            st.header("	:angry:")
+            st.write("Score :")
+            st.progress(float(response["score"]), text = response["score"])
+            st.write("")
+            st.write("")
+
+            st.write("Sentiment :")
+            if response["sentiment"] == "positive" :
+                st.header(":smile:")
+            else :
+                st.header("	:angry:")
 
 
 
 # streamlit run streamlit_app.py
-            
-
-# 
